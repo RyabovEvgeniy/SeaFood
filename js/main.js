540073cb005a330e9dc26b3fa497a91924fd45b6
RyabovEvgeniy <likerus99@gmail.com>
2022-08-20 12:46:29 +0300

    Сверстал блок header

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.header-slider').slick({
+    vertical: true,
+		dots: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="img/next.svg" alt=""></button>'
+  });
+});
