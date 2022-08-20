4df4c4147ddb9c26fb71e1ec5e9a13c8a13c0fb8
RyabovEvgeniy <likerus99@gmail.com>
2022-08-20 18:18:04 +0300

    Сверстал секцию product

--- js/main.js
+++ js/main.js
@@ -5,4 +5,23 @@ $(document).ready(function(){
 		prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.svg" alt=""></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="img/next.svg" alt=""></button>'
   });
+
+	$('.product__name').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		centerMode: true,
+		arrows: true,
+		focusOnSelect: true,
+		asNavFor: '.product__content',
+		vertical: true,
+		prevArrow: '<button type="button" class="product-prev"><img src="img/product-prev.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="product-next"><img src="img/product-next.svg" alt=""></button>'
+	});
+	$('.product__content').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		asNavFor: '.product__name',
+		fade: true,
+		arrows: false,		
+	});
 });
